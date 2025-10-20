8c705b3fb8cbf256f7356da2b5dd22d70e3be2c2
Oscaaari <2oscaaar@gmail.com>
2025-10-20 22:37:18 +0300

    toggle light with F added

--- src/app/clock/page.tsx
+++ src/app/clock/page.tsx
@@ -9,7 +9,10 @@ const megrim = Megrim({ subsets: ['latin'], weight: '400' });
 
 export default function ClockPage() {
     const [time, setTime] = useState<string[]>(["2","2","2","2","2","2"]);
+    const [mouse, setMouse] = useState({ x: 0, y: 0 });
+    const [darknessOn, setDarknessOn] = useState(false);
 
+    //setTime
     useEffect(() => {
         const intervalId = setInterval(() => {
             setTime(new Intl.DateTimeFormat("en-GB", {
@@ -24,19 +27,42 @@ export default function ClockPage() {
         return () => clearInterval(intervalId);
     }, []);
 
+    //setMouse
+    useEffect(() => {
+        const handleMove = (e: MouseEvent) => {
+            setMouse({ x: e.clientX, y: e.clientY });
+        };
+
+        window.addEventListener("mousemove", handleMove);
+        return () => window.removeEventListener("mousemove", handleMove);
+    }, []);
+
+    useEffect(() => {
+        const handleKeyDown = (event: KeyboardEvent) => {
+        if (event.key.toLowerCase() === 'f') {
+            setDarknessOn(prev => !prev);
+        }
+        };
+        window.addEventListener('keydown', handleKeyDown);
+        return () => window.removeEventListener('keydown', handleKeyDown);
+    }, []);
+
+
     return (
-        <div className={styles.mainContainer}>
+        <div className={styles.mainContainer}>            
             <div className={styles.clock}>
-                <div className={`${styles.top} ${megrim.className}`}>
+                <div className={`${styles.top} ${darknessOn ? styles.dark : ''} ${megrim.className}`}>
                     {time.map((digit, i) => (
                         <React.Fragment key={`group-${i}`}>
                             <div className={styles.digitTube}>
                             {Array.from({ length: 10 }, (_, number) => (
                                 <div
-                                key={number}
-                                className={`${styles.digit} ${
-                                    number === +digit ? styles.active : styles.inactive
-                                }`}
+                                    key={number}
+                                    className={`
+                                        ${styles.digit} 
+                                        ${number === +digit ? styles.active : styles.inactive}
+                                        ${darknessOn ? styles.dark : ''}
+                                    `}
                                 >
                                 {number}
                                 </div>
@@ -44,13 +70,19 @@ export default function ClockPage() {
                             </div>
 
                             {(i + 1) % 2 === 0 && i !== time.length - 1 && (
-                            <div key={`sep-${i}`} className={styles.separator}>:</div>
+                                <div key={`sep-${i}`} className={styles.seperator}>:</div>
                             )}
                         </React.Fragment>
                     ))}
                 </div>
                 <div className={styles.base}></div>
             </div>
+            <div 
+                className={styles.darkness}
+                style={{
+                    display: darknessOn ? '' : 'none',
+                }}
+            ></div>
         </div>
     );
 }
