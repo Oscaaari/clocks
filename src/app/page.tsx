ec43031084fc0c0b441001388660a52e517154fd
Oscaaari <2oscaaar@gmail.com>
2025-10-27 10:18:11 +0200

    added a little main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,93 +3,27 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+    <div className={styles.mainContainer}>
+        <a className={styles.previewA} href="/clock/nixie">
+          <div className={styles.previewWrapper}>
+            <Image src="/page-preview/nixie.png" alt="Nixie tube clock" fill className={styles.previewImage}/>
+          </div>
         </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+        <a className={styles.previewA} href="/clock/physical">
+          <div className={styles.previewWrapper}>
+            <Image src="/page-preview/normal-clock.png" alt="Normal clock" fill className={styles.previewImage}/>
+          </div>
         </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+        <a className={styles.previewA} href="/clock/hourglass">
+          <div className={styles.previewWrapper}>
+            <Image src="/page-preview/hourglass.png" alt="Hourglass" fill className={styles.previewImage}/>
+          </div>
         </a>
-      </footer>
+        <a className={styles.previewA} href="/display/bus">
+          <div className={styles.previewWrapper}>
+            <Image src="/page-preview/bus-display.png" alt="Nixie tube clock" fill className={styles.previewImage}/>
+          </div>
+        </a>
     </div>
   );
 }
